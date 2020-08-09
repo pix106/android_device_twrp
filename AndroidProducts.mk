02c7e23930614acf7921278ee7dca15d6551690d
pix106 <sbordenave@gmail.com>
2020-08-09 08:37:30 +0200

    Changes for Lenovo Tab2A720F

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_device.mk
+    $(LOCAL_DIR)/omni_Tab2A720F.mk
--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,17 +1,28 @@
-LOCAL_PATH := device/vendor/device
+LOCAL_PATH := device/lenovo/Tab2A720F
 
 TARGET_ARCH := arm
 TARGET_CPU_ABI := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := cortex-a7
 
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1610612736
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 5955911680
+BOARD_CACHEIMAGE_PARTITION_SIZE := 132120576
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+
 TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel
 BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive
 
 BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/bootimg.mk
+BOARD_MKBOOTIMG_ARGS := --base 0x80000000 --pagesize 2048 --kernel_offset 0x00008000 --ramdisk_offset 0x04000000 --tags_offset 0x00000100 --board 1438361675
 
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
+
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/mt_usb/musb-hdrc.0.auto/gadget/lun%d/file
 
 # TWRP
 #TW_USE_TOOLBOX := true
@@ -26,4 +37,5 @@ TW_INCLUDE_NTFS_3G := true
 TW_THEME := portrait_mdpi
 TW_INCLUDE_FB2PNG := true
 BOARD_SUPPRESS_SECURE_ERASE := true # "Some MMCs have VERY slow secure erase - use standard erase instead"
+TW_CUSTOM_CPU_TEMP_PATH := "/sys/devices/virtual/thermal/thermal_zone1/temp"
 
