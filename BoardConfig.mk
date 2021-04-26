b06912512d362ee619e4e67cf519d3e954aafe15
pix106 <sbordenave@gmail.com>
2021-04-26 09:42:33 +0200

    u65gt: changes so TWRP 3.5.2 fits recovery partition

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,7 +15,7 @@ BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive
 BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/bootimg.mk
 BOARD_MKBOOTIMG_ARGS := --base 0x10000000 --pagesize 2048 --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 
-TWRP_INCLUDE_LOGCAT := true
+#TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
 
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/mt_usb/musb-hdrc.0/gadget/lun%d/file
@@ -29,9 +29,10 @@ TW_DEFAULT_BRIGHTNESS := 128
 TW_NO_REBOOT_BOOTLOADER := true
 #TW_INCLUDE_CRYPTO := true
 RECOVERY_SDCARD_ON_DATA := true
-TW_INCLUDE_NTFS_3G := true
+#TW_INCLUDE_NTFS_3G := true
+BOARD_HAS_NO_REAL_SDCARD := true
 TW_THEME := portrait_mdpi
-TW_INCLUDE_FB2PNG := true
+#TW_INCLUDE_FB2PNG := true
 BOARD_SUPPRESS_SECURE_ERASE := true # "Some MMCs have VERY slow secure erase - use standard erase instead"
 TW_CUSTOM_CPU_TEMP_PATH := "/sys/devices/virtual/thermal/thermal_zone1/temp"
 TW_BRIGHTNESS_PATH := "/sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness"
